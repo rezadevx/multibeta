fc71a347c59b36027005e1269945a704dcb2c741
pinxRobtik <129358594+pinxRobtik@users.noreply.github.com>
2023-09-10 14:52:47 +0700

    update __main__.py

--- ubot/__main__.py
+++ ubot/__main__.py
@@ -14,7 +14,6 @@ async def start_ubot(user_id, _ubot):
         await ubot_.join_chat("xCodee1")
         await ubot_.join_chat("zasupport")
         await ubot_.join_chat("kynansupport")
-        await ubot_.join_chat("aynainhere")
     except asyncio.TimeoutError:
         await remove_ubot(user_id)
         await add_prem(user_id)
