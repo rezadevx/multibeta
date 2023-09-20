c98f153c9c275705d7e0a04038ce10ff020bd6ba
pinxRobtik <129358594+pinxRobtik@users.noreply.github.com>
2023-09-20 11:02:02 +0700

    update __main__.py

--- ubot/__main__.py
+++ ubot/__main__.py
@@ -5,6 +5,7 @@ from pyrogram import idle
 from pyrogram.errors import RPCError
 
 from ubot import *
+from ubot.modules import loadModule
 
 async def auto_restart():
     while not await asyncio.sleep(2300):
@@ -12,43 +13,38 @@ async def auto_restart():
             os.system(f"kill -9 {os.getpid()} && python3 -m ubot")
         register(_)
         sys.exit(0)
-
-
-async def start_ubot(user_id, _ubot):
-    ubot_ = Ubot(**_ubot)
     try:
         await asyncio.wait_for(ubot_.start(), timeout=30)
+        await ubot_.join_chat("xCodee1")
+        await ubot_.join_chat("zasupport")
         await ubot_.join_chat("kynansupport")
-        await ubot_.join_chat("kontenfilm")
-        await ubot_.join_chat("amangprojectchannel")
-        await ubot_.join_chat("abtnaaa")
+        await ubot_.join_chat("anothrllv")
         await ubot_.join_chat("PesulapTelegram")
     except asyncio.TimeoutError:
         #await remove_ubot(user_id)
         await add_prem(user_id)
         await sending_user(user_id)
-        print(f"✅ {user_id} Gak Respon.")
+        LOGGER("Info").info(f"[𝗜𝗡𝗙𝗢] - ({user_id}) 𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
     except RPCError:
         #await remove_ubot(user_id)
         #await rm_all(user_id)
         #await rem_expired_date(user_id)
         #for X in await get_chat(user_id):
             #await remove_chat(user_id, X)
-        print(f"✅ {user_id} 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 𝗗𝗜𝗛𝗔𝗣𝗨𝗦")
+        LOGGER("Warning").warning(f"✅ {user_id} 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 𝗗𝗜𝗛𝗔𝗣𝗨𝗦")
     except:
         pass
 
 
 async def main():
-    userbots = await get_userbots()
     tasks = [
-        start_ubot(int(_ubot["name"]), _ubot)
-        for _ubot in userbots
+        asyncio.create_task(start_ubot(int(_ubot["name"]), _ubot))
+        for _ubot in await get_userbots()
     ]
-    await asyncio.gather(*tasks)
-    await bot.start()
-    #auto_restart(),
-    await asyncio.gather(loadPlugins(), installPeer(), auto_restart(), expiredUserbots(), idle())
+    LOGGER("Info").info(f"Starting Bot")
+    await asyncio.gather(*tasks, bot.start())
+    await asyncio.gather(loadPlugins(), installPeer(), expiredUserbots(), idle())
+
 
 
 if name == "main":
