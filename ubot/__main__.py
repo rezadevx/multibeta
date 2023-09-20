5d0fdf07c7cac277e8136fb9ec0786fcdda530a9
pinxRobtik <129358594+pinxRobtik@users.noreply.github.com>
2023-09-20 12:05:11 +0700

    update __main__.py

--- ubot/__main__.py
+++ ubot/__main__.py
@@ -7,6 +7,16 @@ from pyrogram.errors import RPCError
 from ubot import *
 from ubot.modules import loadModule
 
+
+
+async def auto_restart():
+    while not await asyncio.sleep(1100):
+        def _():
+            os.system(f"kill -9 {os.getpid()} && python3 -m ubot")
+        register(_)
+        sys.exit(0)
+
+
 async def start_ubot(user_id, _ubot):
     ubot_ = Ubot(**_ubot)
     try:
@@ -20,27 +30,28 @@ async def start_ubot(user_id, _ubot):
         #await remove_ubot(user_id)
         await add_prem(user_id)
         await sending_user(user_id)
-        LOGGER("Info").info(f"[𝗜𝗡𝗙𝗢] - ({user_id}) 𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
+        print(f"✅ {user_id} Gak Respon.")
     except RPCError:
         #await remove_ubot(user_id)
         #await rm_all(user_id)
         #await rem_expired_date(user_id)
         #for X in await get_chat(user_id):
             #await remove_chat(user_id, X)
-        LOGGER("Warning").warning(f"✅ {user_id} 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 𝗗𝗜𝗛𝗔𝗣𝗨𝗦")
+        print(f"✅ {user_id} 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 𝗗𝗜𝗛𝗔𝗣𝗨𝗦")
     except:
         pass
 
 
 async def main():
+    userbots = await get_userbots()
     tasks = [
-        asyncio.create_task(start_ubot(int(_ubot["name"]), _ubot))
-        for _ubot in await get_userbots()
+        start_ubot(int(_ubot["name"]), _ubot)
+        for _ubot in userbots
     ]
-    LOGGER("Info").info(f"Starting Bot")
-    await asyncio.gather(*tasks, bot.start())
-    await asyncio.gather(loadPlugins(), installPeer(), expiredUserbots(), idle())
-
+    await asyncio.gather(*tasks)
+    await bot.start()
+    #auto_restart(),
+    await asyncio.gather(loadPlugins(), installPeer(), auto_restart(), expiredUserbots(), idle())
 
 
 if __name__ == "__main__":
